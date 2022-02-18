8d20826a2c5db8e0ea06a941d9438384ca65891b
Russell Gordon <github@russellgordon.ca>
2022-02-18 10:24:58 -0500

    Added a comment to explain what an enumeration is.

--- MultiplicationMaestro/Model/ResultVisibility.swift
+++ MultiplicationMaestro/Model/ResultVisibility.swift
@@ -7,6 +7,14 @@
 
 import Foundation
 
+// An enumeration is like a Bool, or boolean value, in that
+// an enumeration has a defined list of possible values.
+// The advantage is that with an enumeration, we can control
+// what the possible values are.
+
+// ResultVisibility is used to determine what results
+// should be visible in the list of results below the
+// question and answer interface.
 enum ResultVisibility: String {
     case all = "All"
     case incorrect = "Incorrect"
