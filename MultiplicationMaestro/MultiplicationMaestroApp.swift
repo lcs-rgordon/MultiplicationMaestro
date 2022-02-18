032345b36e39fae493ba0285f59011ac1e4be6ba
Russell Gordon <github@russellgordon.ca>
2022-02-18 05:43:10 -0500

    Initial Commit

---
+++ MultiplicationMaestro/MultiplicationMaestroApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationMaestroApp.swift
+//  MultiplicationMaestro
+//
+//  Created by Russell Gordon on 2022-02-18.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationMaestroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
