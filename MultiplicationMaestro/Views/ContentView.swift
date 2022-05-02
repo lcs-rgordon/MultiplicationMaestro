9a393dff90c5b18987f3cd5df32da327ace7f9fb
Russell Gordon <github@russellgordon.ca>
2022-05-01 20:03:53 -0400

    Managed complexity by applying abstraction; created view to show answer and result of checking answer.
    This makes the code in the main view shorter and easier to understand.
    
    It also means we can test the helper view in isolation from the larger program, which makes it easier to write new code.

--- MultiplicationMaestro/Views/ContentView.swift
+++ MultiplicationMaestro/Views/ContentView.swift
@@ -46,33 +46,9 @@ struct ContentView: View {
             Divider()
             
             // Result and input area
-            HStack {
-
-                ZStack {
-                    Image(systemName: "checkmark.circle")
-                        .foregroundColor(.green)
-                        // Only show this when the answer given is correct
-                        //            CONDITION          true  false
-                        .opacity(answerCorrect == true ? 1.0 : 0.0)
-
-                    Image(systemName: "x.square")
-                        .foregroundColor(.red)
-                        // Show this when both of the following situations are true:
-                        // 1. Answer has been checked.
-                        // 2. Answer was not correct.
-                        // Necessary since if we show this only when an answer is incorrect,
-                        // with no other conditions, it would show as soon as a new
-                        // question is generated.
-                        //                 CONDITION1  AND  CONDITION2             true  false
-                        .opacity(answerChecked == true && answerCorrect == false ? 1.0 : 0.0)
-                }
-
-                Spacer()
-                
-                TextField("", text: $inputGiven)
-                    // Ensure input is right-aligned
-                    .multilineTextAlignment(.trailing)
-            }
+            AnswerAndResultView(answerChecked: answerChecked,
+                                answerCorrect: answerCorrect,
+                                inputGiven: $inputGiven)
             
             // Buttons to control program
             ZStack {
---
+++ MultiplicationMaestro/Views/Helpers/AnswerAndResultView.swift
@@ -0,0 +1,55 @@
+//
+//  AnswerAndResultView.swift
+//  MultiplicationMaestro
+//
+//  Created by Russell Gordon on 2022-05-01.
+//
+
+import SwiftUI
+
+struct AnswerAndResultView: View {
+    
+    // MARK: Stored properties
+    let answerChecked: Bool
+    let answerCorrect: Bool
+    @Binding var inputGiven: String
+    
+    // MARK: Computed properties
+    var body: some View {
+        HStack {
+
+            ZStack {
+                Image(systemName: "checkmark.circle")
+                    .foregroundColor(.green)
+                    // Only show this when the answer given is correct
+                    //            CONDITION          true  false
+                    .opacity(answerCorrect == true ? 1.0 : 0.0)
+
+                Image(systemName: "x.square")
+                    .foregroundColor(.red)
+                    // Show this when both of the following situations are true:
+                    // 1. Answer has been checked.
+                    // 2. Answer was not correct.
+                    // Necessary since if we show this only when an answer is incorrect,
+                    // with no other conditions, it would show as soon as a new
+                    // question is generated.
+                    //                 CONDITION1  AND  CONDITION2             true  false
+                    .opacity(answerChecked == true && answerCorrect == false ? 1.0 : 0.0)
+            }
+
+            Spacer()
+            
+            TextField("", text: $inputGiven)
+                // Ensure input is right-aligned
+                .multilineTextAlignment(.trailing)
+        }
+    }
+}
+
+struct AnswerAndResultView_Previews: PreviewProvider {
+    static var previews: some View {
+        AnswerAndResultView(answerChecked: false,
+                            answerCorrect: false,
+                            inputGiven: .constant("77"))
+    }
+}
